56784b0c1a553d770406ea27fe8b0d6c89b63eed
Mahijith <menon.mahijith@gmail.com>
2022-11-14 15:18:51 +0530

    table gets hidden when change is not enough

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ check=document.getElementById('check');
 errorMsg=document.getElementById('errormessage')
 result=document.getElementById('result')
 noOfNotes=document.querySelectorAll('.no-of-notes');
+table=document.getElementById('table');
 availableNotes=[2000,500,200,100,50,10,5,1];
 check.addEventListener('click',function(){
     if(Number(billAmt.value>0) && Number(cashGiven.value>0)){
     if(Number(cashGiven.value)>=Number(billAmt.value)){
    const calculateRemianing=cashGiven.value-billAmt.value;
-   console.log(typeof calculateRemianing)
+   table.style.display="block"
+//    console.log(typeof calculateRemianing)
    calculateChange(calculateRemianing);
     }
     else{
         errorMsg.innerText="You need to Give more Cash";
+        table.style.display="none"
     }
     }
     else{
        errorMsg.innerText="Your Value should be greater than 0";
+       table.style.display="none"
     }
 })
 function calculateChange(amt){
-    errorMsg.innerText=`Remaining Cash to be given back:-${amt}`;
+    errorMsg.innerText=`Remaining Cash to be given back: ${amt}`;
  for(let i=0;i<availableNotes.length;i++){
  const numberOfNotes = Math.trunc(amt/availableNotes[i])
  amt%=availableNotes[i];
