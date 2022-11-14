0c484fb6fb3127e46fd205e372d407e6d01623bc
Mahijith <menon.mahijith@gmail.com>
2022-11-14 15:26:23 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -8,24 +8,36 @@ table=document.getElementById('table');
 availableNotes=[2000,500,200,100,50,10,5,1];
 check.addEventListener('click',function(){
     if(Number(billAmt.value>0) && Number(cashGiven.value>0)){
-    if(Number(cashGiven.value)>=Number(billAmt.value)){
+    if(Number(cashGiven.value)>Number(billAmt.value)){
    const calculateRemianing=cashGiven.value-billAmt.value;
    table.style.display="block"
+   errorMsg.style.fontSize="20px";
+   errorMsg.style.color="green";
 //    console.log(typeof calculateRemianing)
    calculateChange(calculateRemianing);
     }
+    else if(Number(cashGiven.value)==Number(billAmt.value)){
+        errorMsg.innerText="No Need to Give Change";
+        errorMsg.style.fontSize="50px";
+        errorMsg.style.color="black";
+        table.style.display="none"
+    }
     else{
-        errorMsg.innerText="You need to Give more Cash";
+        errorMsg.innerText="You need to take more Cash";
+        errorMsg.style.fontSize="50px";
+        errorMsg.style.color="red";
         table.style.display="none"
     }
     }
     else{
        errorMsg.innerText="Your Value should be greater than 0";
+       errorMsg.style.fontSize="50px";
+     errorMsg.style.color="red";
        table.style.display="none"
     }
 })
 function calculateChange(amt){
-    errorMsg.innerText=`Remaining Cash to be given back: ${amt}`;
+    errorMsg.innerText=`Remaining Cash to be given back : ${amt}`;
  for(let i=0;i<availableNotes.length;i++){
  const numberOfNotes = Math.trunc(amt/availableNotes[i])
  amt%=availableNotes[i];
